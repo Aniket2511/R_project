c5b6150fc7e119b7be5847e489bc52e121ed70b3
Aniket2511 <56197194+Aniket2511@users.noreply.github.com>
2019-10-31 21:29:27 +0530

    Create dataframes.R

---
+++ dataframes.R
@@ -0,0 +1,9 @@
+#dataframes
+title<-c("Star Wars","The Empire Strikes Back","Return of the Jedi",
+         "The Phantom Menace","Attack of the Clones"," Revenge of the Sith",
+         "The Force Awakens")
+year<-c(1977,1980,1983,1999,2002,2005,2015)
+length.min<-c(121,124,133,133,142,140,135)
+box.office.mil<-c(787,534,572,1027,657,849,2059)
+my.data<-(Title=title ,Year=year, Length=length.min, Gross=box.office.mil)
+my.data
